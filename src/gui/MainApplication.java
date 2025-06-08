ae4658ed278a89a6c04b8bf6dd8dc41e3b2c707c
0irem0 <iremmozkaynak@gmail.com>
2025-06-08 09:35:33 +0300

    temizlik

--- src/gui/MainApplication.java
+++ src/gui/MainApplication.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class MainApplication {
     
     public static void main(String[] args) {
-        // Set system look and feel for better integration
+        // Set system look and feel for better integrationz
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
